542ff1837a8aee8cfb62996123b561d487b0c93b
sazan10 <070bex432@ioe.edu.np>
2021-05-28 02:16:09 +0545

    creates modalContainer

--- App.js
+++ App.js
@@ -11,88 +11,52 @@ import type {Node}
 from 'react';
 import {
     SafeAreaView,
-    ScrollView,
     StatusBar,
     StyleSheet,
-    Text,
-    useColorScheme,
     View,
-    Modal,
-    Button,
-    TouchableOpacity
 } from 'react-native';
-import CustomButton from './Components/CustomButton'
-import HeaderText from './Components/HeaderText'
-import {Colors, DebugInstructions, Header, LearnMoreLinks, ReloadInstructions} from 'react-native/Libraries/NewAppScreen';
-import NoteComponent from './Components/NoteComponent';
+import ModalContainer from './Components/ModalCotainer';
+import HeaderText from './Components/HeaderText';
+import CustomButton from './Components/CustomButton';
 
 const App : () => Node = () => {
-    const [modal,
-        setmodal] = useState(false);
-        const isDarkMode =true
+    const [isModal, setisModal] = useState(false);
+    const isDarkMode =true;
+    const modalHeader="Gluten";
+    const dietaryDescription=" Gluten is a hidden allergen and the general name for the proteins found in wheat, barley, and rye products.";
+    const optionDescription="Menu items with gluten in them will show up in your search and menus. Move the slider to filter out or note gluten as an allergen.";
+    const optionHeader="Eat";
     // const isDarkMode = useColorScheme() === 'dark';
-    let backgroundCol=modal?"#d5e4ec":"#f3f3fb";
+    console.log("ismodal", isModal)
+    let backgroundCol=isModal?"#b0b0b0":"#f3f3fb";
     return (
         <SafeAreaView style={{...styles.mainContainer,backgroundColor:backgroundCol}}>
-        <StatusBar
+            <StatusBar
             barStyle={isDarkMode
             ? 'light-content'
             : 'dark-content'}/>
+            <View style={{...styles.sectionContainer,backgroundColor:backgroundCol}}>
+                <HeaderText>
+                    Let's know your dietary preferences.
+                </HeaderText>
+                <HeaderText style={{fontSize:17, color:"#5a638b", paddingTop:10}}>
+                    Any ingredients you don't prefer or are allergic to?
+                </HeaderText>
 
-        
-        <View style={{...styles.sectionContainer,backgroundColor:backgroundCol}}>
-          <View> 
-        <HeaderText>
-        Let's know your dietary preferences.
-        </HeaderText>
-        <HeaderText style={{fontSize:17, color:"#5a638b", paddingTop:10}}>
-              Any ingredients you don't prefer or are allergic to?
-        </HeaderText>
-          </View>
-
-            <CustomButton container_style ={styles.buttonContainer} 
+                <CustomButton container_style ={styles.buttonContainer} 
                         data-test="callComp" 
                         title="save diet"  
                         color="#fff"
-                        pressHandler={() => setmodal(true)} />
-            <Modal
-                animationType={"slide"}
-         
-                transparent={false}
-                onRequestClose={() => setmodal(false)}
-                transparent={true}
-                visible={modal}>
-                <TouchableOpacity  style={{flex:1,opacity:0}} onPressOut={() => {setmodal(false)}}/>          
-                <View style ={styles.modal}>
-                    <HeaderText>
-                      Gluten
-                    </HeaderText>
-                    <HeaderText style={{fontSize:17, color:"#5a638b", paddingTop:10,}}>
-                    Gluten is a hidden allergen and the general name for the proteins found in wheat, barley, and rye products.
-                    </HeaderText>
-                    <NoteComponent title="Eat">
-                      Menu items with gluten in them will show up in your search and menus. Move the slider to filter out or note gluten as an allergen.
-                    </NoteComponent>
-                    <View style={{flex:1, flexDirection:"row", justifyContent:"space-evenly"}}>
-                        <CustomButton container_style ={{...styles.buttonContainer, flex:3, alignItems:"flex-end", margin:10}} 
-                            buttonStyle={{backgroundColor:"#464f7a"}}
-                            data-test="saveComp" 
-                            title="save choice"  
-                            color="#fff"
-                            pressHandler={() => setmodal(false)} />
-                    <CustomButton container_style ={{...styles.buttonContainer,flex:1, alignItems:"flex-start"}} 
-                            buttonStyle={{backgroundColor:"#464f7a", paddingHorizontal:13,paddingVertical:10,borderRadius:40}}
-                            data-test="saveComp" 
-                            title="X"  
-                            color="#fff"
-                            pressHandler={() => setmodal(false)} />
-                    </View>
-                </View>
-     
-            </Modal>
+                        pressHandler={() => setisModal(true)} />
+        <ModalContainer 
+            isModal={isModal}
+            actionClick={()=>{setisModal(false)}} 
+            modalHeader={modalHeader} 
+            dietaryDescription={dietaryDescription}
+            optionDescription={optionDescription}
+            optionHeader={optionHeader}/>    
         </View>
-
-        
+      
         </SafeAreaView>
     );
 };
@@ -110,7 +74,7 @@ const App : () => Node = () => {
 
 const styles = StyleSheet.create({
 
-    modal: {
+    isModal: {
       flex:2.8,
       paddingHorizontal:40,
       paddingTop:40,
