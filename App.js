d06718ebe89eccb25c58c2f422aacae30c95bc7d
Swainstha <swainstha@outlook.com>
2021-05-29 14:40:59 +0545

    Restructure the code in ModalContainer, App, and Slider

--- App.js
+++ App.js
@@ -16,23 +16,18 @@ import {
 } from 'react-native';
 import ModalContainer from './components/ModalContainer';
 import LandingComp from './components/LandingComp';
-import {dummy_preference} from './data/dummy_data'
+import {preference_data} from './data/dummy_data'
 const App  = () => {
     const [isModal, setisModal] = useState(false);
     const isDarkMode =true;
-    const modalHeader="Gluten";
-    const dietaryDescription=" Gluten is a hidden allergen and the general name for the proteins found in wheat, barley, and rye products.";
-    // const optionDescription="Menu items with gluten in them will show up in your search and menus. Move the slider to filter out or note gluten as an allergen.";
-    // const optionHeader="Eat";
-    const [preferenceOption, setpreferenceoption] = useState(dummy_preference[0]);
 
     // const isDarkMode = useColorScheme() === 'dark';
-    let backgroundCol=isModal?"#b0b0b0":"#f3f3fb";
-    const optionSelectHandler=(value)=>{
-        setpreferenceoption(dummy_preference[value])
-
+    const selectionHandler = (choice, type) => {
+        console.log(choice, type);
+        setisModal(false);
     }
-
+    
+    let backgroundCol=isModal?"#b0b0b0":"#f3f3fb";
     return (
         <SafeAreaView style={{...styles.mainContainer,backgroundColor:backgroundCol}}>
             <StatusBar
@@ -42,11 +37,8 @@ const App  = () => {
         <LandingComp backgroundCol={backgroundCol} setisModal={setisModal}>
             <ModalContainer 
                 isModal={isModal}
-                actionClick={()=>{setisModal(false)}} 
-                modalHeader={modalHeader} 
-                preferenceOption={preferenceOption}
-                selectState={optionSelectHandler}
-                dietaryDescription={dietaryDescription}
+                actionClick={selectionHandler} 
+                diet={preference_data}
                 />    
         </LandingComp> 
         </SafeAreaView>
